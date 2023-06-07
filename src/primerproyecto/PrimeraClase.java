926dff9a4c5104a106886d585c9879bc5f46e8f3
Kessi <kessi@koders.cl>
2023-06-06 21:57:36 -0400

    clase

---
+++ src/primerproyecto/PrimeraClase.java
@@ -0,0 +1,39 @@
+package primerproyecto;
+
+import java.util.Scanner;
+
+public class PrimeraClase {
+    public static void main(String[] args) {
+        String variableA;
+        char caracter;
+        Integer numerosEnteros;
+        int variableB;
+        Long numerosGrandesLib;
+        long numerosGrandes;
+        Float decimalesLib;
+        float decimales;
+        Double decimalesGrandesLib;
+        double decimaleGrandes;
+        Boolean logicoLib;
+        boolean logico;
+
+        caracter = 'c';
+        variableA = "test 1";
+        variableB = 4;
+        numerosGrandes = 32874353l;
+        decimaleGrandes = 233.243243255234d;
+        decimales = 84.234734f;
+        decimales = 1f;
+        logico = true;
+        decimaleGrandes = 123.23d;
+
+        System.out.println("Hola Mundo!!! " + variableA + " " + caracter);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa tu edad");
+        variableB = sc.nextInt();
+        System.out.println("tu tienes "+variableB+" años");
+
+
+    }
+}
