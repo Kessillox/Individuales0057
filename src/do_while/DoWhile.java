59caccc14f31dcd3e781bf4176ab0beb02900f9c
diegoaedof <d.aedo.1108@gmail.com>
2023-06-06 22:22:06 -0400

    do while AlfaOmega

---
+++ src/do_while/DoWhile.java
@@ -0,0 +1,12 @@
+package do_while;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        do {
+            System.out.println(contador);
+            contador++;
+        } while(contador <= 10);
+    }
+}
