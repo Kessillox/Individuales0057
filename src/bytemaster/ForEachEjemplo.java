02478ea333582f5335c32ba8df0cd1c2dc4c6ed8
x9laaa <acarreno95@outlook.com>
2023-06-07 19:54:17 -0400

    Comentario de codigo

--- src/bytemaster/ForEachEjemplo.java
+++ src/bytemaster/ForEachEjemplo.java
@@ -4,10 +4,18 @@ public class ForEachEjemplo {
 
     public static void main(String[] args) {
 
+        // Este código es un ejemplo de cómo utilizar un bucle foreach en Java.
+
+        // En este caso, se declara un arreglo de enteros llamado "nombreArray" y se inicializa con valores del 1 al 10.
         int [] nombreArray = {1,2,3,4,5,6,7,8,9,10};
 
+        // En este caso, se declara un arreglo de enteros llamado "nombreArray" y se inicializa con valores del 1 al 10.
         for (int resultado: nombreArray){
+
+            // Dentro del bucle, se imprime en la consola un mensaje que indica el elemento por el que se está pasando.
             System.out.println("Hola voy pasando por el elemento: "+resultado);
         }
+
+        // El resultado será la impresión de los números del 1 al 10 junto con el mensaje.
     }
 }
