f11d6f528c3fa19b11559790b89ab1f9a50e94cf
Andres Sepulveda <bodyweightforce@gmail.com>
2023-06-07 19:33:33 -0400

    Comentarios en do_while

--- src/do_while/DoWhile.java
+++ src/do_while/DoWhile.java
@@ -2,11 +2,16 @@ package do_while;
 
 public class DoWhile {
     public static void main(String[] args) {
+        // iniciamos la variable contador en 0.
         int contador = 0;
 
+        // Realizamos la primera accion del ciclo.
+        // Imprimir por pantalla el valor del contador.
+        // Sumarle 1 al contador cada vez que se ejecute el ciclo.
         do {
             System.out.println(contador);
             contador++;
+            // Repetir el ciclo mientras el contador sea menor o igual a 10.
         } while(contador <= 10);
     }
 }
