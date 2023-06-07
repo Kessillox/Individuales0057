b1d5b7535956d4078fdf8b2fd69992242c435249
Michel Echavarria <mechavaa@triplew.cl>
2023-06-06 22:49:45 -0400

    Ejemplo IF, Java

---
+++ src/grupoada001/Main.java
@@ -0,0 +1,17 @@
+package grupoada001;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        if (numero < 10) {
+            System.out.println("El número es menor a 10.");
+        } else {
+            System.out.println("El número es mayor o igual a 10.");
+        }
+    }
+}
