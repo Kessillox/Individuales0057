8a983954ad18d9f554d635f3ff17701512cb1953
x9laaa <acarreno95@outlook.com>
2023-06-07 22:29:04 -0400

    tarea individual Alex

---
+++ src/bytemaster/Alex/JavaAC/Individual1.java
@@ -0,0 +1,30 @@
+package bytemaster.Alex.JavaAC;
+import java.util.Scanner;
+public class Individual1 {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int cantidad_p, total=0;
+
+            System.out.print("Ingrese la cantidad de personas: ");
+            cantidad_p = sc.nextInt();
+
+            if (cantidad_p > 0){
+
+                if (cantidad_p <= 200){
+                    total = cantidad_p * 4500;}
+                else if (cantidad_p >= 200 && cantidad_p <= 300 ){
+                    total = cantidad_p * 4200;
+                } else if (cantidad_p > 300){
+                    total = cantidad_p * 4000;
+                }
+
+                System.out.print("el valor del evento es $" + total +" para "+ cantidad_p +" personas");
+
+            }
+            else{
+                System.out.print("ingrese un catidad positiva");
+
+            }
+        }
+}
