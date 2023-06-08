f3bd8266ef4ce1e85d8f4ed3fcef79e5e1fcc1b2
unknown <juandalucio@gmail.com>
2023-06-07 22:59:14 -0400

    Ejercicio2 JD

--- src/Grupo_Ada/Juan_Lucio/JavaJD/Ejercicio1.java
+++ src/Grupo_Ada/Juan_Lucio/JavaJD/Ejercicio1.java
@@ -15,14 +15,11 @@ public class Ejercicio1 {
             System.out.print("Escribir la pregunta o tema en la barra de búsqueda: ");
             String busqueda = scanner.nextLine();
 
-            // Reemplaza los espacios en blanco con %20 para la URL
             String busquedaURL = busqueda.replace(" ", "%20");
 
-            // Realiza la búsqueda en Google (puedes cambiar la URL según tu motor de búsqueda preferido)
             String url = "https://www.google.com/search?q=" + busquedaURL;
 
             try {
-                // Abre la página de búsqueda en el navegador predeterminado
                 Desktop.getDesktop().browse(new URI(url));
             } catch (IOException | URISyntaxException e) {
                 System.out.println("No se pudo abrir el navegador.");
@@ -33,8 +30,6 @@ public class Ejercicio1 {
             System.out.println("Seleccionar el resultado más relevante");
             System.out.println("Acceder a la página web correspondiente");
             System.out.println("Leer");
-
-            // Aquí puedes agregar lógica adicional para procesar la información obtenida de la página web
 
             System.out.println("FIN");
         }
