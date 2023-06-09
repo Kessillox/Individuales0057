fbad3b53258f895252ba4579ab2f74b1bad18dda
Joshua Vargas Chia <jvargaschia@gmail.com>
2023-06-09 18:55:38 -0400

    Ejercicios individuales Mod4 Ind3

---
+++ src/rocketeam/Joshua_Vargas/jv_java/Mod4_Ind3.java
@@ -0,0 +1,28 @@
+package rocketeam.Joshua_Vargas.jv_java;
+
+import java.util.Scanner;
+
+public class Mod4_Ind3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el número de personas: ");
+
+        int numeroPersonas = scanner.nextInt();
+        int costoPorPersona;
+
+        if (numeroPersonas > 300) {
+            costoPorPersona = 4000;
+            System.out.println("El número de personas es mayor a 300, el costo por persona es 4000");
+        } else if (numeroPersonas >= 200 && numeroPersonas <= 300) {
+            costoPorPersona = 4200;
+            System.out.println("El número de personas es entre 200 y 300, el costo por persona es 4200");
+        } else {
+            costoPorPersona = 4500;
+            System.out.println("El número de personas es menor a 300, el costo por persona es 4500");
+        }
+        int presupuesto = numeroPersonas * costoPorPersona;
+        System.out.println("El presupuesto para " + numeroPersonas + " personas es de: $" + presupuesto);
+        scanner.close();
+    }
+}
