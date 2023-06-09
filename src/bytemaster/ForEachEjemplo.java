d1e6bb05916622952028637a080a00fb8e6cc2ea
miguel.vargas.gonzalez@gmail.com <miguel.vargas.gonzalez@gmail.com>
2023-06-08 21:51:02 -0400

    merge mv

--- src/bytemaster/ForEachEjemplo.java
+++ src/bytemaster/ForEachEjemplo.java
@@ -1,4 +1,4 @@
-package Bytemaster;
+package bytemaster;
 
 public class ForEachEjemplo {
 
--- src/bytemaster/miguel_vargas/java_mv/mod4_ind3.java
+++ src/bytemaster/miguel_vargas/java_mv/mod4_ind3.java
@@ -1,4 +1,4 @@
-package Bytemaster.Miguel_Vargas.java_mv;
+package bytemaster.miguel_vargas.java_mv;
 import java.util.Scanner;
 
 public class mod4_ind3 {
