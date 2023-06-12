551b1a691701fa12ba8838d8587b6be4b10450bd
pita3012 <sjavieraandrea@gmail.com>
2023-06-11 22:02:03 -0400

    agregando tarea individual 9

--- src/los_malvekexxxx/javierasanchez/java/EjercicioIndividual9.java
+++ src/los_malvekexxxx/javierasanchez/java/EjercicioIndividual9.java
@@ -1,2 +1,18 @@
-package los_malvekexxxx.javierasanchez.java;public class EjercicioIndividual9 {
+package los_malvekexxxx.javierasanchez.java;
+import java.util.Scanner;
+
+public class EjercicioIndividual9 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Ingrese un número entero: ");
+            int numero = scanner.nextInt();
+
+            System.out.println("Los divisores del número " + numero + " son:");
+            for (int i = 1; i <= numero; i++) {
+                if (numero % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
 }
