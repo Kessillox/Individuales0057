03be2b307eb07b47bed928bf75501d7eb935d6cd
OsaroAR <132012515+OsaroAR@users.noreply.github.com>
2023-06-23 22:12:32 -0400

    Trabajo individual 15 y 16 OA

--- src/los_malvekexxxx/oscar_aguilera/java_oa/M4TrabajoIndividual16.java
+++ src/los_malvekexxxx/oscar_aguilera/java_oa/M4TrabajoIndividual16.java
@@ -1,2 +1,31 @@
-package los_malvekexxxx.oscar_aguilera.java_oa;public class M4TrabajoIndividual16 {
+package los_malvekexxxx.oscar_aguilera.java_oa;
+
+import java.util.Scanner;
+
+public class M4TrabajoIndividual16 {
+    public static void main(String[] args) {
+        String clave = "qwerty123";
+        int intentos = 3;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (intentos > 0) {
+            System.out.print("Introduzca su contraseña: ");
+            String contraseña = sc.nextLine();
+
+            if (contraseña.equals(clave)) {
+                System.out.println("La contraseña ingresada es correcta");
+                System.out.println("**Bienvenido**");
+                break; // Sale del bucle while si la clave es correcta
+            } else {
+                intentos--;
+                System.out.println("La contraseña ingresada es incorrecta. Le quedan " + intentos + " intentos.");
+            }
+        }
+        System.out.println();
+        if (intentos == 0) {
+            System.out.println("Ha excedido los intentos posibles, acceso bloqueado.");
+        }
+
+    }
 }
