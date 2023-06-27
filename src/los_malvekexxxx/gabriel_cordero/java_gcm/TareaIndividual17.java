f1f62157be38fe3e8363679092f25eedfd29d40c
Gabrielcorderomutinelli@gmail.com <gabrielcorderomutinelli@gmail-com>
2023-06-27 19:58:06 -0400

    Trabajo individual 17

---
+++ src/los_malvekexxxx/gabriel_cordero/java_gcm/TareaIndividual17.java
@@ -0,0 +1,33 @@
+package los_malvekexxxx.gabriel_cordero.java_gcm;
+
+import java.util.Scanner;
+
+public class TareaIndividual17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        do {
+            System.out.println("Ingresa una frase: ");
+            input = scanner.nextLine().trim();
+        } while (input.isEmpty());
+
+        int vocalCount = 0;
+        int consonantCount = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = Character.toLowerCase(input.charAt(i));
+
+            if (Character.isLetter(ch)) {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vocalCount++;
+                } else {
+                    consonantCount++;
+                }
+            }
+        }
+
+        System.out.println("Cantidad de vocales: " + vocalCount);
+        System.out.println("Cantidad de consonantes: " + consonantCount);
+    }
+}
