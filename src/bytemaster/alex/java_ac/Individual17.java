53bb3cee82bef83ce25e93a001dc1d49f3366b73
x9laaa <acarreno95@outlook.com>
2023-06-27 21:36:59 -0400

    individual 17 y 19

---
+++ src/bytemaster/alex/java_ac/Individual17.java
@@ -0,0 +1,33 @@
+package bytemaster.alex.java_ac;
+
+import java.util.Scanner;
+
+public class Individual17 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String palabra;
+        int countC = 0;
+        int countV = 0;
+
+        do {
+            System.out.println("Ingrese la palabra");
+            palabra = sc.next();
+
+        }
+        while(palabra.equals(null));
+
+        for (int i = 0; i < palabra.length(); i++) {
+            char caracter = palabra.charAt(i);
+            if (caracter == 'a'|| caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u' ||
+                    caracter == 'A'|| caracter == 'E' || caracter == 'I' || caracter == 'O' || caracter == 'U'){
+                countV++;
+            }else {
+                countC++;
+            }
+            System.out.println(caracter);
+        }
+        System.out.println("el numero de Vocales es :"+countV);
+        System.out.println("el numero de consonantes es :"+countC);
+    }
+}
