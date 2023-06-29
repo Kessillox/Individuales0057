3d8c59884bfca4d4a72975cf6db7b0b2aea20689
Andres Sepulveda <bodyweightforce@gmail.com>
2023-06-28 21:07:44 -0400

    Agregando Loggers a la clase Main dentro de mainapp

--- src/primerproyecto/mainapp/Main.java
+++ src/primerproyecto/mainapp/Main.java
@@ -6,10 +6,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.logging.Logger;
 
 
 public class Main {
     public static void main(String[] args) {
+
+        Logger log = Logger.getLogger(Main.class.getName());
 
         Persona pers1 = new Persona();
         LocalDate fNac = LocalDate.now();
@@ -20,11 +23,22 @@ public class Main {
         pers1.setFechaNac(fNac);
 
 
-        System.out.println(pers1);
-        System.out.println(pers1.getNombre());
-        System.out.println("LocalDate = " + LocalDate.now());
-        System.out.println("LocalDateTime = " + LocalDateTime.now());
-        System.out.println("LocalTime = " + LocalTime.now());
-        System.out.println("El Cumpleaños de Andrés es = " + LocalDate.of(1995, Month.MARCH, 4));
+        //System.out.println(pers1);
+        log.info(pers1.toString());
+
+        //System.out.println(pers1.getNombre());
+        log.info(pers1.getNombre());
+
+        //System.out.println("LocalDate = " + LocalDate.now());
+        log.info("LocalDate = " + LocalDate.now());
+
+        //System.out.println("LocalDateTime = " + LocalDateTime.now());
+        log.info("LocalDateTime = " + LocalDateTime.now());
+
+        //System.out.println("LocalTime = " + LocalTime.now());
+        log.info("LocalTime = " + LocalTime.now());
+
+        //System.out.println("El Cumpleaños de Andrés es = " + LocalDate.of(1995, Month.MARCH, 4));
+        log.info("El cumpleaños de Andres es = " + LocalDate.of(1995, Month.MARCH, 4));
     }
 }
