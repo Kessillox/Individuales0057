15b1744a232c26382d3eca549cdfbde79eecf275
x9laaa <acarreno95@outlook.com>
2023-06-29 20:31:29 -0400

    individual 21

---
+++ src/bytemaster/individuales/EjercicioInd21.java
@@ -0,0 +1,27 @@
+package bytemaster.individuales;
+
+public class EjercicioInd21 {
+    public static void main(String[] args) {
+
+        int algo = 5;
+        String var1 = "En un lugar de la Mancha, de cuyo\n" +
+                "nombre no quiero acordarme, no ha mucho tiempo que vivía un\n" +
+                "hidalgo de los de lanza en astillero, adarga antigua, rocín flaco\n" +
+                "y galgo corredor. ";
+
+        var1 += "Una olla de algo más vaca que carnero,\n" +
+                "salpicón las más noches, duelos y quebrantos los sábados,\n" +
+                "lentejas los viernes, algún palomino de añadidura los domingos,\n" +
+                "consumían las tres partes de su hacienda.";
+
+        /* El resto della concluían sayo de velarte, calzas de
+        velludo para las fiestas, con sus pantuflos de lo mesmo,
+        y los días de entresemana se honraba con su
+        vellorí de lo más fino */
+
+        int otraCosa = 6;
+        System.out.println("El valor de la primera variable es " + algo + " y de la segunda es " + otraCosa);
+
+        System.out.println("¿Te cuento un cuento?: " + var1);
+    }
+}
---
+++ src/bytemaster/leonardo_lopez/psc_llc/EjercicioIndividual21.java
@@ -0,0 +1,26 @@
+package Clase11;
+
+public class Ejemplo1 {
+    public static void main(String args[]) {
+        int algo = 5;
+        String var1 = "En un lugar de la Mancha, de cuyo\n" +
+                      "nombre no quiero acordarme, no ha mucho tiempo que vivía un\n" +
+                      "hidalgo de los de lanza en astillero, adarga antigua, rocín flaco\n" +
+                      "y galgo corredor. ";
+
+        var1 += "Una olla de algo más vaca que carnero,\n" +
+                 "salpicón las más noches, duelos y quebrantos los sábados,\n" +
+                 "lentejas los viernes, algún palomino de añadidura los domingos,\n" +
+                 "consumían las tres partes de su hacienda.";
+
+        /* El resto della concluían sayo de velarte, calzas de
+        velludo para las fiestas, con sus pantuflos de lo mesmo,
+        y los días de entresemana se honraba con su
+        vellorí de lo más fino */
+        
+        int otraCosa = 6;
+        System.out.println("El valor de la primera variable es " + algo + " y de la segunda es " + otraCosa);
+
+        System.out.println("¿Te cuento un cuento?: " + var1);
+    }
+}
