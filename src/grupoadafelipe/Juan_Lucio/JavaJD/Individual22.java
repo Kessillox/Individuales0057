1c59574445661c427fc61d3a2748d30e3cf8ee0d
unknown <juandalucio@gmail.com>
2023-06-30 22:20:25 -0400

    actualización ejercicios semana 26/06

---
+++ src/grupoadafelipe/Juan_Lucio/JavaJD/Individual22.java
@@ -0,0 +1,18 @@
+public class Individual22 {
+    public static void main(String[] args) {
+        int algo = 5;
+        String var1 =   "En un lugar de la Mancha, de cuyo " +
+                        "nombre no quiero acordarme, no hace mucho tiempo vivía un " +
+                        "hidalgo de los de lanza en astillero, adarga antigua, rocín flaco " +
+                        "y galgo corredor. " +
+                        "Una olla de algo más vaca que carnero, " +
+                        "salpicón las más noches, duelos y quebrantos los sábados, " +
+                        "lentejas los viernes, algún palomino de añadidura los domingos, " +
+                        "consumían las tres partes de su hacienda.";
+
+        int otraCosa = 6;
+
+        System.out.println("El valor de la primera variable es " + algo + " y de la segunda es " + otraCosa);
+        System.out.println("¿Te cuento un cuento?: " + var1);
+    }
+}
