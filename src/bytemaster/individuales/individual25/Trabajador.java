4a0b33872b56e1328b911a9a9d69612a44445a3f
x9laaa <acarreno95@outlook.com>
2023-07-03 21:25:25 -0400

    agrego get y set de class tarbajador

--- src/bytemaster/individuales/individual25/Trabajador.java
+++ src/bytemaster/individuales/individual25/Trabajador.java
@@ -7,6 +7,46 @@ public class Trabajador {
     private int telefono;
     private int edad;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRun() {
+        return run;
+    }
+
+    public void setRun(String run) {
+        this.run = run;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
     public Trabajador() {
     }
 
