4016cc6ec548800b39cff24e3625a6dbbbd1246f
lopezpalacios25 <36944983+lopezpalacios25@users.noreply.github.com>
2023-07-03 21:39:05 -0400

    Individual 23

---
+++ src/rocketeam/christopher_lopez/java_cl/Individual23.java
@@ -0,0 +1,41 @@
+package rocketeam.christopher_lopez.java_cl;
+
+import java.util.Scanner;
+
+public class Individual23 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String textoUsuario = "";
+        String txtResultado = "";
+        System.out.println("Ingrese una cadena de texto a su gusto");
+
+        textoUsuario = scanner.nextLine();
+
+
+        for (int indice = 0; indice < textoUsuario.length(); indice++){
+
+            System.out.println(textoUsuario.charAt(indice));
+
+            char caracterActual = textoUsuario.charAt(indice);
+
+            if(Character.isWhitespace(caracterActual) || Character.isLetterOrDigit(caracterActual) ){
+
+                if (Character.isLowerCase(caracterActual))
+
+                    caracterActual = Character.toUpperCase(caracterActual);
+                else
+
+                    caracterActual = Character.toLowerCase(caracterActual);
+
+                txtResultado = txtResultado + caracterActual;
+            }
+        }
+
+        System.out.println(txtResultado);
+
+    }
+
+}
