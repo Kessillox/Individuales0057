99a24375f55f5792ae27fcdc502670bf22b4a54c
fvmolina21 <fvmolina@uc.cl>
2023-07-03 21:45:44 -0400

    cambio t1

--- src/bytemaster/individuales/individual25/main.java
+++ src/bytemaster/individuales/individual25/main.java
@@ -4,8 +4,9 @@ public class main {
     public static void main(String[] args) {
         Trabajador osvaldo1=new Trabajador("osvaldo","toro","1238547-k",9568791,60);
 
-        osvaldo1.toString();
+        System.out.println(osvaldo1.toString(););
         System.out.println(osvaldo1.descomponerRun());
+        System.out.println(osvaldo1.nombreCompleto());
     }
 
 }
