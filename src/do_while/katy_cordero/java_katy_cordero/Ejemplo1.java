a483dc3b9fa4e2cdcb051603bfd627e3b091dd0c
Andres Sepulveda <bodyweightforce@gmail.com>
2023-07-03 21:05:38 -0400

    12

--- src/do_while/katy_cordero/java_katy_cordero/Ejemplo1.java
+++ src/do_while/katy_cordero/java_katy_cordero/Ejemplo1.java
@@ -1,42 +1,20 @@
 package do_while.katy_cordero.java_katy_cordero;//nombre del package debe comenzar con minúscula
 
-<<<<<<< HEAD
 public class Ejemplo1 {//el nombre de clase debe comenzar con mayúscula y seguir la notación CamelCase
 
-        public static void main (String[] args) {	//se corrige sintaxis del argumento
+                public void main(String[] args) { //se corrige sintaxis del argumento
+                    int algo = 5; //nombre de variable no puede comenzar con símbolos
+                    String var1 = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. " +
+                            "Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda."; //nombre de variable con notación camelCase pero la primera letra debe ser minúscula
 
-            int algo = 5;	//nombre de variable no puede comenzar con símbolos
-
-            String var1 = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. " +
-                    "Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda."; //nombre de variable con notación camelCase pero la primera letra debe ser minúscula
-//El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mesmo,
-=======
-public class Ejemplo1 { //el nombre de clase debe comenzar con mayúscula y seguir la notación CamelCase
-
-        public static void main (String[] args) { //se corrige sintaxis del argumento
-            int algo = 5; //nombre de variable no puede comenzar con símbolos
-            String var1 = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. " +
-                    "Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda."; //nombre de variable con notación camelCase pero la primera letra debe ser minúscula
-
-            //El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mesmo,
->>>>>>> f92cde8 (se sube individual 21)
+                    //El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mesmo,
 //y los días de entresemana se honraba con su vellorí de lo más fino
 
-            int otraCosa;
-            otraCosa = 6;
-<<<<<<< HEAD
-            System.out.println("El valor de la primera variable es "
-                    + algo + " y de la segunda es " + otraCosa); 	//se quita el “$” de la variable “algo”
+                    int otraCosa = 6;
+                    System.out.println("El valor de la primera variable es "
+                            + algo + " y de la segunda es " + otraCosa);    //se quita el “$” de la variable “algo”
 
-            System.out.println("¿Te cuento un cuento?: " + var1);	// nombre de variable empieza con minúscula
-        }
-    }
+                    System.out.println("¿Te cuento un cuento?: " + var1);    // nombre de variable empieza con minúscula
+                }
+            }
 
-
-=======
-            System.out.println("El valor de la primera variable es " + algo + " y de la segunda es " + otraCosa); //se quita el “$” de la variable “algo”
-            System.out.println("¿Te cuento un cuento?: " + var1); // nombre de variable empieza con minúscula
-
-        }
-    }
->>>>>>> f92cde8 (se sube individual 21)
