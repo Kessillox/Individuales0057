6bc20c429127a2e67caaf6ba8d05bfe4e6a67c68
osvaldotoromontecinos@hotmail.com <osvaldotoromontecinos@hotmail.com>
2023-07-06 19:33:15 -0400

    ind27v2

--- src/bytemaster/individuales/individual27/Trabajador.java
+++ src/bytemaster/individuales/individual27/Trabajador.java
@@ -9,33 +9,63 @@ public class Trabajador {
     private String run;
     private int telefono;
     private int edad;
-    private List<CargaFamiliar> listaCargaFamiliar = new ArrayList<>();
 
     public Trabajador() {
     }
 
-    public Trabajador(String nombre, String apellido, String run, int telefono, int edad, List<CargaFamiliar> listaCargaFamiliar) {
+    public Trabajador(String nombre, String apellido, String run, int telefono, int edad) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.run = run;
         this.telefono = telefono;
         this.edad = edad;
-        this.listaCargaFamiliar = listaCargaFamiliar;
     }
 
-    @Override
-    public String toString() {
-        return "Trabajador{" +
-                "nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", run='" + run + '\'' +
-                ", telefono=" + telefono +
-                ", edad=" + edad +
-                '}';
+    public String getNombre() {
+        return nombre;
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRun() {
+        return run;
+    }
+
+    public void setRun(String run) {
+        this.run = run;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
     public String nombreCompleto() {
-        return nombre  +" "+  apellido;
+        String nC = getNombre()+" "+getApellido();
+        return nC;
     };
+
     public  int descomponerRun() {
         String[] partes = run.split("-"); // Dividir el RUN en dos partes separadas por el guion
         String parteAnterior = partes[0]; // Obtener la primera parte del RUN antes del guion
