cd9e10db73b420c2eb722b3222e028a95b08218b
alopezgo <alopezgo1991@gmail.com>
2023-07-05 22:34:26 -0400

    se crea package ind25

---
+++ src/codingclub/andrealopez/java/ind25/Trabajador.java
@@ -0,0 +1,81 @@
+package codingclub.andrealopez.java.ind25;
+
+public class Trabajador {
+
+    private String nombres, apellidos, run;
+    private int telefono, edad;
+
+    public Trabajador() {
+    }
+
+    public Trabajador(String nombres, String apellidos, String run, int telefono, int edad) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.run = run;
+        this.telefono = telefono;
+        this.edad = edad;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getRun() {
+        return run;
+    }
+
+    public void setRun(String run) {
+        this.run = run;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Corredora{" +
+                "nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", run='" + run + '\'' +
+                ", telefono=" + telefono +
+                ", edad=" + edad +
+                '}';
+    }
+
+    public String nombreCompleto(){
+        return this.nombres + " " + this.apellidos;
+    }
+
+    public int descomponerRun(){
+        String runCompleto = this.run.replace(".", "");
+        String [] runArray = runCompleto.split("-");
+        int rut = Integer.parseInt(runArray[0]);
+        return rut;
+
+    }
+}
