2ba29768de48713167ab86c6a0858fc69241ed7d
Andres Sepulveda <bodyweightforce@gmail.com>
2023-07-07 20:09:49 -0400

    inainsoi

--- src/primerproyecto/poo/Profesor.java
+++ src/primerproyecto/poo/Profesor.java
@@ -40,15 +40,11 @@ public class Profesor extends Persona{
                 "} ";
     }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 03f24c580d5bb5f7f194341fdb4688f0cede44a3
     public String presentacion(){
       String saludo = "Hola! soy el profesor "+this.getNombre()+" y hago clases de "+this.asignatura +" en los cursos "+ this.getListaCursos();
     return saludo;
     }
-<<<<<<< HEAD
 
 
     public String inspectoria(){
@@ -56,6 +52,4 @@ public class Profesor extends Persona{
         return  orden;
     }
 
-=======
->>>>>>> 03f24c580d5bb5f7f194341fdb4688f0cede44a3
 }
