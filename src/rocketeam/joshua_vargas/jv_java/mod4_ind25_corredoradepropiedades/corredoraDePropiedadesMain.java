f068c2d6095c3f3950209d76742f0514c2c5cff1
Joshua Vargas Chia <jvargaschia@gmail.com>
2023-07-13 21:54:27 -0400

    Correcion tipo de dato teléfono

--- src/rocketeam/joshua_vargas/jv_java/mod4_ind25_corredoradepropiedades/corredoraDePropiedadesMain.java
+++ src/rocketeam/joshua_vargas/jv_java/mod4_ind25_corredoradepropiedades/corredoraDePropiedadesMain.java
@@ -2,7 +2,7 @@ package rocketeam.joshua_vargas.jv_java.mod4_ind25_corredoradepropiedades;
 
 public class corredoraDePropiedadesMain {
     public static void main(String[] args) {
-        Trabajador worker1 = new Trabajador("Pedrito", "Alvarez", "17848508-3", "932447901", 32);
+        Trabajador worker1 = new Trabajador("Pedrito", "Alvarez", "17848508-3", 932447901, 32);
         System.out.println(worker1.descomponerRun());
         System.out.println(worker1.nombreCompleto());
         System.out.println(worker1);
